00e16a358a3cea6e10c1e3b057b8b1905ad20bd0
Aman <amanbhangu1@gmail.com>
2017-05-19 23:20:31 -0400

    Image as a button and make second screen

--- app/src/main/java/com/example/taranjotsidhu/tinder/MainActivity.java
+++ app/src/main/java/com/example/taranjotsidhu/tinder/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.taranjotsidhu.tinder;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,15 +9,24 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
-
+ImageView imgButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
+        imgButton = (ImageView)findViewById(R.id.imageButton2);
+        imgButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),Second.class);
+                startActivity(intent);
+            }
+        });
     }
 
     @Override
