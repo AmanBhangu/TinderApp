8f7fab7b7025d4aa023171de32a9deefd182b742
1691524 <amanbhangu1@gmail.com>
2017-05-20 12:42:51 -0400

    fixing errors

--- app/src/main/java/com/example/taranjotsidhu/tinder/Second.java
+++ app/src/main/java/com/example/taranjotsidhu/tinder/Second.java
@@ -8,6 +8,8 @@ import android.os.Handler;
 import android.view.MotionEvent;
 import android.view.View;
 
+import static android.R.attr.id;
+
 /**
  * An example full-screen activity that shows and hides the system UI (i.e.
  * status bar and navigation/system bar) with user interaction.
@@ -89,23 +91,10 @@ public class Second extends AppCompatActivity {
 
         setContentView(R.layout.activity_second);
 
-        mVisible = true;
-        mControlsView = findViewById(R.id.fullscreen_content_controls);
-        mContentView = findViewById(R.id.fullscreen_content);
 
 
-        // Set up the user interaction to manually show or hide the system UI.
-        mContentView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                toggle();
-            }
-        });
 
-        // Upon interacting with UI controls, delay any scheduled hide()
-        // operations to prevent the jarring behavior of controls going away
-        // while interacting with the UI.
-        findViewById(R.id.dummy_button).setOnTouchListener(mDelayHideTouchListener);
+     
     }
 
     @Override
