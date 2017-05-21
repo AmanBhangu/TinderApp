c78d145ea710a3a92e55ad40b4775371f1371131
1691524 <amanbhangu1@gmail.com>
2017-05-21 10:24:29 -0400

    fixing bugs

--- app/src/main/java/com/example/taranjotsidhu/tinder/Second.java
+++ app/src/main/java/com/example/taranjotsidhu/tinder/Second.java
@@ -1,13 +1,24 @@
 package com.example.taranjotsidhu.tinder;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class Second extends AppCompatActivity {
-
+    ImageView imgButton3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
     }
+    public void messages(View view) {
+        Intent intent = new Intent(this,Third.class);
+        startActivity(intent);
+    }
 }
+
+
+
+
--- app/src/main/java/com/example/taranjotsidhu/tinder/Third.java
+++ app/src/main/java/com/example/taranjotsidhu/tinder/Third.java
@@ -1,7 +1,9 @@
 package com.example.taranjotsidhu.tinder;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Third extends AppCompatActivity {
 
@@ -9,5 +11,15 @@ public class Third extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_third);
+    }
+
+        public void image(View view) {
+            Intent intent = new Intent(this, Fourth.class);
+            startActivity(intent);
+        }
+
+    public void mes(View view) {
+        Intent intent = new Intent(this,Fifth.class);
+        startActivity(intent);
     }
 }
